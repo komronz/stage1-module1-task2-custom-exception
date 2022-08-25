ab72d1a67256a63e76517b0a850cd4e0ca2fcfa9
komronz <pandacoder0820@gmail.com>
2022-08-25 12:13:08 +0500

    progressONe

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -2,11 +2,6 @@ package com.epam.mjc;
 
 public class StudentManager {
 
-  static class StudentNotFoundException extends IllegalArgumentException{
-    public StudentNotFoundException(String message){
-      super(message);
-    }
-  }
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
@@ -21,10 +16,15 @@ public class StudentManager {
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
     manager.find(1000);
+    try {
       for (int i = 0; i < IDs.length; i++) {
         Student student = manager.find(IDs[i]);
         System.out.println("Student name " + student.getName());
       }
+    }
+    catch (NullPointerException e){
+      System.out.println(e);
+    }
 
   }
 }
