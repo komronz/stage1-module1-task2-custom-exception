32ae8d320d0541052ee4da0e6fb8ba6fafc01752
komronz <pandacoder0820@gmail.com>
2022-08-25 12:22:35 +0500

    progressFOR4

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -1,10 +1,7 @@
 package com.epam.mjc;
 
 public class StudentManager {
-
-
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-
   public Student find(long studentID) throws StudentNotFoundException{
     Student id =  Student.getValueOf(studentID);
     if (id == null){
@@ -12,19 +9,12 @@ public class StudentManager {
     }
     return id;
   }
-
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
     manager.find(1000);
-    try {
       for (int i = 0; i < IDs.length; i++) {
         Student student = manager.find(IDs[i]);
         System.out.println("Student name " + student.getName());
       }
-    }
-    catch (NullPointerException e){
-      System.out.println(e);
-    }
-
   }
 }
